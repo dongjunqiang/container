7747efb0d1953b4e419379b6eeb1cd91eef63d7e
Rezha Julio <rezhajulio@users.noreply.github.com>
2017-03-28 15:24:25 +0700

    Create chroot.c

---
+++ src/chroot.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+#include "common.h"
+
+int main(int argc, char *argv[]) {
+	ensure_argc(3, argc);
+
+	ensure(chroot(argv[1]));
+	ensure(chdir("/"));
+
+	ensure(chain_command(2, argc, argv));
+}
